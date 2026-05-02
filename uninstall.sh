7f78890d0afeaceef22e68c68b78ba851cf81222
Adil Hanney <adilhanney@disroot.org>
2026-05-02 23:35:36 +0100

    chore: allow running uninstall.sh multiple times without error

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-flatpak remove -y --delete-data studio.affinity.Affinity
+rm -rf ~/.var/app/studio.affinity.Affinity/data/wineprefix/
 rm -rf builddir repo
+
+flatpak remove -y --delete-data studio.affinity.Affinity 2>/dev/null
